93b945c4715686c7ff39b49806761aedbade33c6
Dilan Sheth <ds1619@ic.ac.uk>
2021-03-27 15:24:46 +0000

    Added last time updated field

--- src/pages/LivePrice.js
+++ src/pages/LivePrice.js
@@ -3,17 +3,27 @@ import {useState} from 'react'
 import {Prices} from '../components/Prices'
 
 export function LivePricePage() {
-  const [gbp, setGbp] = useState(null)
+  const [gbp, setGbp] = useState(null);
+  const [usd, setUsd] = useState(null);
+  const [eur, setEur] = useState(null);
+  const [lastUpdated, setLastUpdated] = useState(null);
+
   function refreshPrices() {
     fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
     .then((response) => response.json())
-    .then((json) => setGbp(json.bpi.GBP.rate))
+    .then((json) => {
+      setGbp(json.bpi.GBP.rate);
+      setUsd(json.bpi.USD.rate);
+      setEur(json.bpi.EUR.rate);
+      setLastUpdated(new Date().toLocaleTimeString());
+    })
   }
   return (
     <div>
       <h1>Live bitcoin price</h1>
-      <Prices usd={123} eur={1234} gbp={gbp} />
+      <Prices usd={usd} eur={eur} gbp={gbp} />
       <button onClick={refreshPrices}>Refresh prices</button>
+      {lastUpdated && <p>Last update: {lastUpdated}</p>}
     </div>
   )
 }
